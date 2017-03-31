9df7109e1aa4ebfaa791c8a6f9838263fda57a00
easy-ui <npm.uiconsole@gmail.com>
2017-03-31 17:19:48 +0200

    Add Json management

--- app/src/main/java/com/msa/ui/Constants.java
+++ app/src/main/java/com/msa/ui/Constants.java
@@ -19,18 +19,4 @@ public class Constants {
     public interface BUNDLE {
         String RSS_URL = "rssURL";
     }
-
-    /*
-    public interface URL {
-        String RSS_LE_MONDE         = "http://www.lemonde.fr/rss/une.xml";
-        String RSS_NICE_MATIN       = "http://www.nicematin.com/ville/cote-d-azur/rss";
-        String RSS_LE_PARISIEN      = "http://www.leparisien.fr/une/rss.xml";
-        String RSS_LES_ECHOS        = "https://www.lesechos.fr/rss/rss_articles_journal.xml";
-        String RSS_LOBS             = "http://tempsreel.nouvelobs.com/rss.xml";
-        String RSS_SCIENCE_AVENIR   = "https://www.sciencesetavenir.fr/rss.xml";
-        String RSS_FR_ANDROID       = "";
-        String RSS_ANDROID_MT       = "http://feeds.feedburner.com/AndroidMtNews?format=xml";
-    }
-    */
-
 }
